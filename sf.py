53be1687d756aca501e4f4428f45f62d68ce5f2f
van1sh <van1sh@protonmail.com>
2025-03-06 21:28:46 +0000

    reducing the docker size

--- sf.py
+++ sf.py
@@ -91,7 +91,7 @@ def main() -> None:
             'cors_origins': [],
         }
 
-        p = argparse.ArgumentParser(description="SpiderFoot {__version__}: Open Source Intelligence Automation.")  # Define p first
+        p = argparse.ArgumentParser(description="SpiderFoot __version__ : Open Source Intelligence Automation.")  # Define p first
         p.add_argument("-d", "--debug", action='store_true', help="Enable debug output.")
         p.add_argument("-l", "--listen", metavar="IP:port", help="IP and port to listen on.")
         p.add_argument("-m", metavar="mod1,mod2,...", type=str, help="Modules to enable.")
@@ -118,7 +118,7 @@ def main() -> None:
         args = p.parse_args()  # Parse arguments after defining p
 
         if args.version:
-            print("SpiderFoot {__version__}: Open Source Intelligence Automation.")  # Removed f-string as no place holders are used.
+            print("SpiderFoot __version__: Open Source Intelligence Automation.")  # Removed f-string as no place holders are used.
             sys.exit(0)
 
         if args.max_threads:
