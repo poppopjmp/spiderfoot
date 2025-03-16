2f43606294d5f298dc321c3b7d0bd525c6c3d5a0
van1sh <van1sh@protonmail.com>
2025-03-16 01:06:30 +0100

    Add Mandiant TI and Luminar modules

---
+++ test/unit/modules/test_sfp_mandiant_ti.py
@@ -0,0 +1,48 @@
+import unittest
+from modules.sfp_mandiant_ti import sfp_mandiant_ti
+from sflib import SpiderFoot
+from spiderfoot import SpiderFootEvent, SpiderFootTarget
+
+class TestModuleMandiantTI(unittest.TestCase):
+
+    def setUp(self):
+        self.default_options = {
+            '_fetchtimeout': 5,
+            '_useragent': 'SpiderFoot',
+            '_dnsserver': '8.8.8.8',
+            '_internettlds': 'https://publicsuffix.org/list/effective_tld_names.dat',
+            '_internettlds_cache': 72
+        }
+        self.sf = SpiderFoot(self.default_options)
+        self.module = sfp_mandiant_ti()
+        self.module.setup(self.sf, dict())
+
+    def test_handleEvent(self):
+        target_value = 'example.com'
+        target_type = 'DOMAIN_NAME'
+        target = SpiderFootTarget(target_value, target_type)
+        self.sf.target = target
+
+        event_type = 'DOMAIN_NAME'
+        event_data = 'example.com'
+        event_module = 'test_module'
+        source_event = SpiderFootEvent(event_type, event_data, event_module, None)
+
+        self.module.opts['api_key'] = 'test_api_key'
+        self.module.handleEvent(source_event)
+
+        self.assertTrue(self.module.results)
+
+    def test_query(self):
+        self.module.opts['api_key'] = 'test_api_key'
+        result = self.module.query('example.com')
+        self.assertIsNotNone(result)
+
+    def test_producedEvents(self):
+        self.assertEqual(self.module.producedEvents(), ['THREAT_INTELLIGENCE'])
+
+    def test_watchedEvents(self):
+        self.assertEqual(self.module.watchedEvents(), ['DOMAIN_NAME', 'INTERNET_NAME', 'IP_ADDRESS'])
+
+if __name__ == '__main__':
+    unittest.main()
