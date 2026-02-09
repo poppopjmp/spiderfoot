f1c013ab2ca30fcdbab590056d7422f26d69d339
van1sh <van1sh@protonmail.com>
2026-02-09 17:39:12 +0100

    v5.60.0 - Cycle 61: Inter-service authentication (static + HMAC tokens)

--- test/unit/test_sfcli.py
+++ test/unit/test_sfcli.py
@@ -29,7 +29,7 @@ class TestSpiderFootCliEssential(TestModuleBase):
         self.cli = SpiderFootCli()
         # Disable spool to avoid file issues
         self.cli.config['cli.spool'] = False
-        self.cli.version = "5.59.0"
+        self.cli.version = "5.60.0"
 
     def tearDown(self):
         """Clean up after each test method."""
