99ae98ceea06885dbe75c9e24c1b38caeea31a29
van1sh <van1sh@protonmail.com>
2026-02-09 20:27:45 +0100

    v5.96.0 - RC Cycle 8: Remove deprecated version field from all 3 docker-compose files

--- spiderfoot/__version__.py
+++ spiderfoot/__version__.py
@@ -10,7 +10,7 @@ def _get_version():
         return version_string
     except (FileNotFoundError, IOError):
         # Fallback version if VERSION file is not found
-        return "5.95.0"
+        return "5.96.0"
 
 __version__ = _get_version()
 
