9d1d800add7dbe7f50a0578e8bc29af6187525bc
van1sh <van1sh@protonmail.com>
2026-02-10 08:05:21 +0100

    v5.129.0 - RC Cycle 48: Add missing __init__.py to scan_service package

---
+++ spiderfoot/scan_service/__init__.py
@@ -0,0 +1,9 @@
+"""Scan service package.
+
+Contains :class:`SpiderFootScanner`, the core scan engine that orchestrates
+module loading, event dispatching, and scan lifecycle management.
+"""
+
+from spiderfoot.scan_service.scanner import SpiderFootScanner
+
+__all__ = ["SpiderFootScanner"]
